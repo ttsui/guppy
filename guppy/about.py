e79a810d3955f5f63a7954f11a02cd1234bbd5f4
Tony Tsui <tsui.tony@gmail.com>
2005-10-18 13:37:12 +0000

    Update version number for 0.0.5 release
    git-archimport-id: tsui.tony@gmail.com--projects/guppy--dev--0.0.1--patch-54

--- guppy/about.py
+++ guppy/about.py
@@ -16,7 +16,7 @@
 ## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 APP_NAME = 'Guppy'
-VERSION = '0.9.0'
+VERSION = '0.0.5'
 DESCRIPTION='Topfield PVR File Downloader'
 AUTHOR = 'Tony Tsui'
 AUTHOR_EMAIL = 'tsui.tony@gmail.com'
